80fb1a7e744a1a667d25398fd7960d543bde6a3a
GNadiola <gnadiola@gmail.com>
2024-09-15 23:27:53 +0000

    Q2

--- M2HW1_Grosvenor.cpp
+++ M2HW1_Grosvenor.cpp
@@ -9,33 +9,86 @@
 #include <iomanip>  // for setprecision()
 using namespace std;
 
-int main() {
-  // Set up our variables
+int main() 
+{
+  // Question 1
+  //Set up our variables
   string  name;
-  int     yr_released;
-  double  amount_grossed_in_box_office;
+  double  starting_account_balance;
+  double  account_balance;
+  double  amount_of_deposit;
+  double  amount_of_withdrawal;
 
   // Ask questions
-  cout << "What is the name of the movie?" << endl;
-  cout << "What year did it come out? " << endl;
-  cout << "How much did it gross in the box office?" << endl;
+  cout << "What is your name?" << endl;
+  cin >> name;
+  cout << "What is your starting account balance? " << endl;
+  cin >> starting_account_balance;
+  cout << "How much do you wish to deposit?" << endl;
+  cin >> amount_of_deposit;
+  cout << "How much do you wish to withdraw?" << endl;
+  cin >> amount_of_withdrawal;
   
   // Opening
   cout << "_________________________________________" << endl;
-  cout << "The movie Endgame";
-  cout << name << " was an interesting movie!" << endl;
+  cout << "The name associated with this account is " << name << endl;
+  cout << "Your account number is 24861964" << endl;
+
+  //Math
+  account_balance=starting_account_balance-amount_of_withdrawal+amount_of_deposit;
+
+  //Response
+  cout << "Your final account balance is " << account_balance << endl;
 
   // set all decimals to 2 places
   cout << fixed << setprecision(2);
 
-  // Give the answers
-  cout << "It was released in 2019." << endl;
-  cout << "In the box office, this movie grossed 2.798 billion dollars worldwide." << endl;
 
-  // Give facts
-  cout << "This was Marvel's highest grossing movie." << endl;
-  cout << "Marvel had a fake version of the script that they called \"Code Blue.\"" << endl;
-  cout << "Bruce Banner's explanation of time travel in the movie actually came straight from a quantum physicist." << endl;
-  cout << "One of the most well recieved moments from the movie was when Captain America said his famous phrase \"Avengers Assemble\". " << endl;
+  //Question 2
+  //Set up Variables
+    int length;
+    int width;
+    int height;
+    int volume; // in cubic feet
+
+// the price related variables - all in $
+const double COST_PER_CU_FT = 0.3;
+const double PRICE_PER_CU_FT = 0.5;
+double cost;
+double customer_price;
+double profit;
+
+    // say hi
+    /*
+    cout << "Hi, what's your name? ";
+    cin >> name;
+    cout << "Nice to meet you, " << name << endl;
+    */
+
+    // Ask user questions
+    cout << "Let's get the crate dimensions (in feet)." << endl;
+    cout << "What's the length? ";
+    cin >> length;
+    cout << "What's the width? ";
+    cin >> width;
+    cout << "Finally, what's the height? ";
+    cin >> height;
+
+    // Calculations
+    volume = length * width * height;
+    // cost calculations
+    cost           = volume * COST_PER_CU_FT;
+    customer_price = volume * PRICE_PER_CU_FT;
+    profit = customer_price - cost;
+
+    // Output
+    // next line requires iomanip
+    cout << setprecision(2) << fixed;
+    cout << "Crate is: " << width << " x " << length << " x " << height << endl;
+    cout << "Total volume is: " << volume << " cubic feet." << endl;
+    // output the cost, the customer price, and the profit
+    cout << "The total cost for production is $" << cost << endl;
+    cout << "The amount that the customer will be charged is $" << customer_price << endl;
+    cout << "The amount of profit gained is $" << profit << endl;
 
 }
