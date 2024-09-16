5b43827b834980d765e0446324be9b3d8cb83db1
GNadiola <gnadiola@gmail.com>
2024-09-16 14:59:15 +0000

    M3T2 Done

---
+++ Module 3/M3T2_Grosvenor.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+ // CSC 134
+ //  M3T1
+ // Ask the user to enter the length and width of two rectangles,
+ // and then display the area of both
+ // GrosvenorN
+ // 9/16/2024
+
+ int main() {
+    // set up variables
+    double width1, width2, length1, length2;
+    double area1, area2;
+
+    cout << "M3T1 - Area of two rectangles" << endl;
+    cout << "Width of the first rectangle:" << endl;
+    cin >> width1;
+    cout << "Length of first rectangle:" << endl;
+    cin >> length1;
+    cout << "Width of the second rectangle:" << endl;
+    cin >> width2;
+    cout << "Length of the second rectangle:" << endl;
+    cin >> length2;
+
+   // Calc areas
+   area1 = length1 * width1;
+   area2 = length2 * width2;
+
+   // print the answer
+   cout << "The first area is: " << area1 << endl;
+   cout << "Te second area is: " << area2 << endl;
+
+   // Tell which is larger
+   if (area1 > area2) {
+      cout << "The first rectangle is larger." << endl;
+   }
+   else if (area1 < area2) {
+      cout << "The second rectangle is larger." << endl;
+   }
+   else { 
+      // area1 == area2
+      cout << "The rectangles are the same size." << endl;
+   }
+
+   
+ }
