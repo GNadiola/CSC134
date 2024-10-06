3aa5cf5a0e054888dd1f1fd5bff64ed25e8e1226
GNadiola <gnadiola@gmail.com>
2024-10-06 16:37:35 +0000

    M3HW1 Q1 DONE

--- M3HW1_Grosvenor.cpp
+++ M3HW1_Grosvenor.cpp
@@ -4,3 +4,25 @@
 // 9/25/2024
 
 #include <iostream>
+using namespace std;
+
+ int main() {
+    // set up variables
+    int answer
+
+    cout << "Hello, I am a C++ program!" << endl;
+    cout << "Do you like me? Please type yes or no." << endl;
+    cin >> answer;
+
+    // Output options
+    if (answer == yes) {
+      cout << "That is great! I am sure we will get along." << endl;
+    }
+    else if (answer == no) {
+      cout << "Well, maybe you will learn to like me later." << endl;
+   }
+   else { 
+      cout << "If you are not sure… that is OK." << endl;
+   }
+   
+ }
