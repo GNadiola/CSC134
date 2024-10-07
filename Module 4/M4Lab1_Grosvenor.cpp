95c9523b2e53c61bce243f185715e9e7e70cf586
GNadiola <gnadiola@gmail.com>
2024-10-07 14:43:21 +0000

    File Import and M4Lab1 Start

---
+++ Module 4/M4Lab1_Grosvenor.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+//CSC 134
+// M4LAB1
+//GrosvenorN
+// 10/7/2024
+
+int main() {
+    cout << "M4LAB1" << endl;
+    string emoji = "👽";
+    // cout << "👽" << endl;
+    // Print sideways (row)
+    for (int i = 0; i < 9; i++) {
+        cout << "👽";
+    }
+    cout << endl; // finish the row
+
+// print down (column)
+}
