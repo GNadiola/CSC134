2e82c2ba4f6f68581b751963e700ffa4b78258ff
GNadiola <gnadiola@gmail.com>
2024-10-28 14:53:24 +0000

    M5T1 and Module 5 Organization

---
+++ Module 5/M5T1_Grosvenor.cpp
@@ -0,0 +1,37 @@
+// CSC 134
+// M5T1 - Basic Functions
+// GrosvenorN
+//10/28/2024
+
+#include <iostream>
+using namespace std;
+
+// function prototypes (short version)
+void say_hello();
+int give_the_answer();
+int double_an_int(int number);
+
+// main
+int main() {
+    cout << "Hello from main" << endl;
+    say_hello();
+    cout << "The magic numbe is: ";
+    int number = give_the_answer();
+    cout << number << endl;
+    int twotimes = double_an_int(number);
+    cout << "Double that is " << twotimes << endl;
+    return 0;
+}
+
+// function definitions (full version)
+void say_hello() {
+    cout << "Hello from say_hello()" << endl;
+}
+int give_the_answer() {
+    int answer = 42;
+    return answer;
+}
+int double_an_int(int number) {
+    int twotimes = number * 2;
+    return twotimes;
+}
