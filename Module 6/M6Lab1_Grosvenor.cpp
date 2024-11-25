f6aecd2e9495d1f05492c8709855878765f024aa
GNadiola <gnadiola@gmail.com>
2024-11-25 16:09:18 +0000

    M6Lab1 Edits

--- Module 6/M6Lab1_Grosvenor.cpp
+++ Module 6/M6Lab1_Grosvenor.cpp
@@ -12,9 +12,14 @@ const int BLACK = 0;  // Blank
 
 int main() {
     // load the chamber
-    vector<int> chamber = {RED, BLACK, RED};
+    vector<int> chamber = {};
     // cout << chamber << endl; // NOTE: This doesn't work!
     // use a range based loop to print all the rounds instead
+
+    chamber.push_back(RED);
+    chamber.push_back(RED);
+    chamber.push_back(BLACK);
+
     for (int round: chamber) {
         if (round == RED) {
             cout << "RED ";
@@ -24,7 +29,5 @@ int main() {
         }
     }
     cout << endl;
-    
-
-
+    return 0;
 }
